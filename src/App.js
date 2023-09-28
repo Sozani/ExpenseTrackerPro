be5485ccdbebc2b1fbf10eb2c3542a08d4fc97c8
Suzan <sys612603@gmail.com>
2023-09-28 11:19:41 +0300

    add heading

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Hello in My app</h1>
+    </div>
+  );
 }
 
 export default App;
