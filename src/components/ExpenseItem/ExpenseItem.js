117e9df3054e09a333c43580b7c0ca44a5b9930a
Suzan <sys612603@gmail.com>
2023-09-29 02:12:41 +0300

    Add rest files

--- src/components/ExpenseItem/ExpenseItem.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./ExpenseItem.css";
-const ExpenseItem = () => {
-  return <div>ExpenseItem</div>;
-};
-
-export default ExpenseItem;
---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import ExpensesChart from "./ExpensesChart";
+import ExpensesList from "./ExpensesList";
+import Card from "../UI/Card";
+import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </div>
+  );
+};
+export default Expenses;
